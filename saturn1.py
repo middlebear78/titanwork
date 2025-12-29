7f0f7873494bd87a1c85e0b4f03362996dbbb181
uri shamir <urisham@gmail.com>
2025-12-29 02:50:57 +0200

    i added a sum variable and printed it in the end after the loop

--- saturn1.py
+++ saturn1.py
@@ -1,7 +1,8 @@
 print("hello world mother fuckers")
 question = input("what is your name? ")
 print (f"Your name is {question}")
+sum = 0 
 for char in question:
     print (char)
-
-
+    sum += 1
+print(sum)
